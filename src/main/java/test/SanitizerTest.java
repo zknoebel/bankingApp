78acab98c3be96287c2f04383852152bffdfd505
zknoebel <zknoebel@hawaii.edu>
2017-10-10 19:54:07 -1000

    include junit in bankingApp

---
+++ src/main/java/test/SanitizerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SanitizerTest {
+
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+}
